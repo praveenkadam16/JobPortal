d197805b5f1bc01a44e54858cecdd4a3dce0c374
Kadam <2204805@cognizant.com>
2025-08-13 10:37:23 +0530

    Added log

--- src/main/java/com/portal/jobportal/controller/UsersController.java
+++ src/main/java/com/portal/jobportal/controller/UsersController.java
@@ -2,6 +2,7 @@ package com.portal.jobportal.controller;
 
 import java.util.List;
 
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,14 +16,17 @@ import com.portal.jobportal.services.UsersTypeService;
 
 @Controller
 public class UsersController {
+	    Logger log;
 		private final UsersTypeService usersTypeService;
 		@Autowired
 		public UsersController(UsersTypeService usersTypeService) {
 			this.usersTypeService=usersTypeService;
+			
 		}
 		@GetMapping("/register")
 		public String register(Model model) {
 			List<UsersType> usersType=usersTypeService.getAll();
+			System.out.println("****Inside Usercontroller service******");
 			model.addAttribute("getAllTypes",usersType);
 			model.addAttribute("user",new Users());
 			return "register";
