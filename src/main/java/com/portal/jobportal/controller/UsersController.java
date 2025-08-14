71bd68603a882144cbf2aaa1f82503e3d315c582
Kadam <2204805@cognizant.com>
2025-08-14 17:40:43 +0530

    added @postmapping for User registration

--- src/main/java/com/portal/jobportal/controller/UsersController.java
+++ src/main/java/com/portal/jobportal/controller/UsersController.java
@@ -7,10 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.portal.jobportal.entity.Users;
 import com.portal.jobportal.entity.UsersType;
+import com.portal.jobportal.services.UsersService;
 import com.portal.jobportal.services.UsersTypeService;
+
+import jakarta.validation.Valid;
 
 
 
@@ -18,9 +22,11 @@ import com.portal.jobportal.services.UsersTypeService;
 public class UsersController {
 	    Logger log;
 		private final UsersTypeService usersTypeService;
+		private final UsersService usersService;
 		@Autowired
-		public UsersController(UsersTypeService usersTypeService) {
+		public UsersController(UsersTypeService usersTypeService,UsersService usersService) {
 			this.usersTypeService=usersTypeService;
+			this.usersService=usersService;
 			
 		}
 		@GetMapping("/register")
@@ -31,5 +37,10 @@ public class UsersController {
 			model.addAttribute("user",new Users());
 			return "register";
 			
+		}
+		@PostMapping("/register/new")
+		public String userRegistration(@Valid Users users) {
+			usersService.addNew(users);
+			return "dashboard";
 		}
 }
